a372eeb4f2506164051e996c941d7f125abe9626
Vaasudev <vaasudev96@gmail.com>
2020-11-03 19:13:06 +0530

    Revert "PACS Jigsaw evaluation."
    This reverts commit 50f373ba0d327f43a42809218eb66e2e5a76f789.

--- run_PACS_photo.sh
+++ run_PACS_photo.sh
@@ -1,4 +1,5 @@
 # python train_jigsaw.py --batch_size 128 --n_classes 7 --learning_rate 0.001 --network resnet18 --val_size 0.1 --folder_name test --jigsaw_n_classes 30 --train_all --image_size 222 --min_scale 0.8 --max_scale 1.0 --random_horiz_flip 0 --jitter 0 --tile_random_grayscale 0.1 --source art cartoon photo --target sketch --jig_weight 0.9 --bias_whole_image 0.7
+<<<<<<< HEAD
 
 
 
@@ -12,3 +13,10 @@ python train_jigsaw.py --batch_size 128 --n_classes 7 --learning_rate 0.001 --ne
 
 # python train_jigsaw_evaljig.py --batch_size 128 --n_classes 7 --learning_rate 0.001 --network resnet18 --val_size 0.1 --folder_name test --jigsaw_n_classes 30 --train_all --min_scale 0.8 --max_scale 1.0 --random_horiz_flip 0.5 --jitter 0.4 --tile_random_grayscale 0.1 --source art cartoon sketch --target photo --jig_weight 0.7 --bias_whole_image 0.9 --image_size 222 --epochs 100 \
 # --jig_only
+=======
+# python train_jigsaw.py --batch_size 128 --n_classes 7 --learning_rate 0.001 --network resnet18 --val_size 0.1 --folder_name test --jigsaw_n_classes 30 --train_all --image_size 222 --min_scale 0.8 --max_scale 1.0 --random_horiz_flip 0 --jitter 0 --tile_random_grayscale 0.1 --source art cartoon photo --target sketch --jig_weight 0.9
+
+# python train_jigsaw.py --batch_size 128 --n_classes 7 --learning_rate 0.001 --network resnet18 --val_size 0.1 --folder_name test --jigsaw_n_classes 30 --train_all --min_scale 0.8 --max_scale 1.0 --random_horiz_flip 0.5 --jitter 0.4 --tile_random_grayscale 0.1 --source photo art cartoon --target sketch --jig_weight 0.7 --bias_whole_image 0.9 --image_size 222 --epochs 100
+python train_jigsaw.py --batch_size 128 --n_classes 7 --learning_rate 0.001 --network resnet18 --val_size 0.1 --folder_name test --jigsaw_n_classes 30 --train_all --min_scale 0.8 --max_scale 1.0 --random_horiz_flip 0.5 --jitter 0.4 --tile_random_grayscale 0.1 --source art cartoon sketch --target photo --jig_weight 0.7 --bias_whole_image 0.9 --image_size 222 --epochs 100
+# python train_jigsaw.py --batch_size 128 --n_classes 7 --learning_rate 0.001 --network resnet18 --val_size 0.1 --folder_name test --jigsaw_n_classes 30 --train_all --min_scale 0.8 --max_scale 1.0 --random_horiz_flip 0.5 --jitter 0.4 --tile_random_grayscale 0.1 --source photo art cartoon --target sketch --jig_weight 0.7 --bias_whole_image 0.9 --image_size 222
+>>>>>>> parent of 50f373b... PACS Jigsaw evaluation.
